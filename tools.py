7cf728121c03985743b8f7ea896128e1c7c736a1
mikkimat81539 <102390057+mikkimat81539@users.noreply.github.com>
2026-04-29 01:11:28 -0500

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,27 +1,13 @@
 # Function for user selecting bandit level
 def levelChoice():
-	passwordList = []
-	try:
-		lvlInput = int(input("Enter a bandit level between 0 - 34: "))
+	lvlInput = int(input("Enter a bandit level between 0 - 34: "))
 		
-		if lvlInput > 34:
-			print("Invalid response, input numbers between 0 - 34")
-			return sshConnection() # applied recursion to loop back to start of function
+	if lvlInput > 34:
+		print("Invalid response, input numbers between 0 - 34")
+		return lvlChoice() # applied recursion to loop back to start of function
 
-		else:
-			with open("banditPasswords.txt") as file:
-				for i in file:
-					passwordList.append(i)	
-			
-			try:
-				joinPasswords = "".join(passwordList[lvlInput])
-				print(joinPasswords)
-
-			except IndexError:
-				print("Password does not exist")			
-	except ValueError:
-		print("Invalid response")
-		return sshConnection()
+	else:
+		os.system(f"ssh bandit{lvlInput}@bandit.labs.overthewire.org -p 2220")			
 
 # MAIN LOOP
 while True:
